54e6d330950f35c1bb9b0ac16c2df724aec61e4a
TomasManueltm <antoniomanuelbaptistabaptista@gmail.com>
2022-05-05 16:09:09 +0100

    projeto tomasmanuel

--- index.js
+++ index.js
@@ -1,44 +1,37 @@
+// recursos
 let $ = (e) => document.querySelector(e);
-
-// pegar tela
 var carouselItem = $(".carouselItem");
 
-carouselItem.innerHTML = "<h2>Tomas Manuel</h2> <p>@Criador<p>";
 
 
 
-
-
+// controles
 const controls = {
     item : 0,
     tmp : 0,
     segundo : 10,
 
     frasesItem : [
-        {autor: "Github-tomasmanuel", msg: "Projeto interativo"},
-        {autor: "tomas", msg: "Na minha criação tinha de tudo e um pouco mais"},
-        {autor: "tomasmanuel", msg: "Pprojeto criado em 05/05/2022 | 01h10 - 02h50"},
-        {autor: "Proposito", msg: "Espero que usas sua criativade para melhorar este meu projeto"}
+        {autor: "Github-tomasmanueltm", msg: "Projeto interativo"},
+        {autor: "Desenvolvimento", msg: "Html, Sass, Js"},
+        {autor: "Sobre", msg: "Na minha criação tinha de tudo e um pouco mais"},
+        {autor: "Meu proposito", msg: "Espero que usas sua criativade para melhorar este meu projeto"}
     ]
 }
 
-contTempo = ()=>{
-   
-
-    if(controls.tmp == 10)
-    {
+// funcao
+carouselStart = ()=>{
+    if(controls.tmp == 10){
         controls.tmp = 0
         
-        if(controls.item == controls.frasesItem.length)
-        {
+        if(controls.item == controls.frasesItem.length){
                 controls.item = 0
         }
-        controls.item++
-       
         carouselItem.innerHTML =  `<h2>${controls.frasesItem[controls.item].msg}</h2><p>@${controls.frasesItem[controls.item].autor}<p>`;
+        controls.item++
     }
     controls.tmp++
 }
 
-
-setInterval(()=>{contTempo()}, 300);
+// conometro
+setInterval(()=>{carouselStart()}, 300);
